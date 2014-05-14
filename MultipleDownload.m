4110fbc22f7e54e044a48198074b857f5efdc976
amit kumar <amitkumar3968@gmail.com>
2014-05-14 14:48:20 +0530

    Update MultipleDownload.m

--- MultipleDownload.m
+++ MultipleDownload.m
@@ -7,31 +7,54 @@
 #import "MultipleDownload.h"
 
 
+
+
 @implementation MultipleDownload
 
 @synthesize urls, requests, receivedDatas, finishCount;
-
+@synthesize Oncompletion2;
+@synthesize AssociatedDataArray;
+@synthesize requestsArray;
 - init {
     if ((self = [super init])) {
 		self.finishCount = 0;
 		NSMutableArray *array = [[NSMutableArray alloc] init];
 		self.receivedDatas = array;
-		[array release];
-		
+//		[array release];
+        
 		NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
 		self.requests = dict;
-		[dict release];
-
+        self.requestsArray=[NSMutableArray new];
+        
+//		[dict release];
+        
     }
     return self;
 }
 
-- (void)dealloc {
-	[urls release];
-	[requests release];
-	[receivedDatas autorelease];
-    [super dealloc];
+
+-(id)initWithBlock:(completionhandler2)theblock AndDataUrl:(NSArray *)thearray AndUrlArray:(NSArray *)urlArray{
+    if ((self = [super init])) {
+		self.finishCount = 0;
+		NSMutableArray *array = [[NSMutableArray alloc] init];
+		self.receivedDatas = array;
+        
+        
+		NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
+		self.requests = dict;
+        
+        self.Oncompletion2=theblock;
+        self.AssociatedDataArray=thearray;
+           self.requestsArray=[NSMutableArray new];
+        
+        
+        [self requestWithUrls:urlArray];
+        
+        
+    }
+    return self;
 }
+
 
 - (void)setDelegate:(id)val
 {
@@ -52,18 +75,19 @@
 }
 
 - (void)requestWithUrls:(NSArray *)aUrls {
-
+    
 	[receivedDatas removeAllObjects];
 	[requests removeAllObjects];
-	[urls autorelease];
+    [requestsArray removeAllObjects];
+//	[urls autorelease];
 	urls = [aUrls copy];
-	
+    
 	for(NSInteger i=0; i< [urls count]; i++){
 		NSMutableData *aData = [[NSMutableData alloc] init];
 		[receivedDatas addObject: aData];
-		[aData release];
-		
-		NSURLRequest *request = [[NSURLRequest alloc] 
+//		[aData release];
+        
+		NSURLRequest *request = [[NSURLRequest alloc]
 								 initWithURL: [NSURL URLWithString: [urls objectAtIndex:i]]
 								 cachePolicy: NSURLRequestReloadIgnoringLocalCacheData
 								 timeoutInterval: 10
@@ -71,10 +95,13 @@
 		NSURLConnection *connection = [[NSURLConnection alloc]
 									   initWithRequest:request
 									   delegate:self];
-		
+        
 		[requests setObject: [NSNumber numberWithInt: i] forKey: [NSValue valueWithNonretainedObject:connection]];
-		[connection release];
-		[request release];	
+        
+        [self.requestsArray addObject:connection];
+        
+//		[connection release];
+//		[request release];
 	}
 }
 
@@ -83,14 +110,16 @@
 }
 
 - (NSString *)dataAsStringAtIndex:(NSInteger)idx {
-	return [[[NSString alloc] initWithData:[receivedDatas objectAtIndex:idx] encoding:NSUTF8StringEncoding] autorelease];
+	return [[NSString alloc] initWithData:[receivedDatas objectAtIndex:idx] encoding:NSUTF8StringEncoding];
 }
 
 #pragma mark NSURLConnection Delegates
 - (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
 {
 	NSInteger i = [[requests objectForKey: [NSValue valueWithNonretainedObject:connection]] intValue];
-    [[receivedDatas objectAtIndex:i] setLength:0];
+    
+    NSMutableData *tdata=(NSMutableData *)[receivedDatas objectAtIndex:i];
+    [tdata setLength:0];
 }
 
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
@@ -103,19 +132,38 @@
 {
 	NSInteger i = [[requests objectForKey: [NSValue valueWithNonretainedObject:connection]] intValue];
 	finishCount++;
-	
-	if ([delegate respondsToSelector:@selector(didFinishDownload:)])
-        [delegate performSelector:@selector(didFinishDownload:) withObject: [NSNumber numberWithInt: i]];
-	
-	if(finishCount >= [urls count]){
-		if ([delegate respondsToSelector:@selector(didFinishAllDownload)])
-			[delegate didFinishAllDownload];
-	}
+     NSMutableData *tdata=(NSMutableData *)[receivedDatas objectAtIndex:i];
+    
+    NSLog(@"-----+++++++++%@",[[self.AssociatedDataArray objectAtIndex:i] objectForKey:@"filname"]);
+     [tdata writeToFile:[[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0]stringByAppendingFormat:@"/%@.mp3",[[self.AssociatedDataArray objectAtIndex:i] objectForKey:@"filname"]] atomically:YES];
+    
+    self.Oncompletion2(    [self.AssociatedDataArray objectAtIndex:i]);
+    
+    
+//	if ([delegate respondsToSelector:@selector(didFinishDownload:)])
+//        [delegate performSelector:@selector(didFinishDownload:) withObject: [NSNumber numberWithInt: i]];
+//    
+//	if(finishCount >= [urls count]){
+//		if ([delegate respondsToSelector:@selector(didFinishAllDownload)])
+//			[delegate didFinishAllDownload];
+//	}
 }
 
 - (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
-	if ([delegate respondsToSelector:@selector(didFailDownloadWithError:)])
+	/*
+    if ([delegate respondsToSelector:@selector(didFailDownloadWithError:)])
         [delegate performSelector:@selector(didFailDownloadWithError:) withObject: error];
+
+     */
 }
 
-@end
+
+-(void)cancelAllrequests{
+    
+    NSLog(@"%@",self.requestsArray);
+    for (NSURLConnection *Tconn in self.requestsArray) {
+        
+        [Tconn cancel];
+    }
+    
+}
