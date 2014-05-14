21847627e226104df296692996fbd5a131f28b5e
amit kumar <amitkumar3968@gmail.com>
2014-05-14 14:47:34 +0530

    Update MultipleDownload.h

--- MultipleDownload.h
+++ MultipleDownload.h
@@ -1,19 +1,18 @@
-//
-//  MultipleDownload.h
-//
-//  Copyright 2008 Stepcase Limited.
-//
-
-#import <UIKit/UIKit.h>
-
+typedef void (^completionhandler2)(NSDictionary *);
 
 @interface MultipleDownload : NSObject {
 	NSMutableArray *urls;
 	NSMutableDictionary *requests;
+    NSMutableArray *requestsArray;
 	NSMutableArray *receivedDatas;
 	NSInteger finishCount;
 	id delegate;
 }
+@property(nonatomic,copy)completionhandler2 Oncompletion2;
+@property(nonatomic,strong)NSArray *AssociatedDataArray;
+
+@property (nonatomic,strong)    NSMutableArray *requestsArray;
+
 
 @property (nonatomic,retain) NSMutableArray *urls;
 @property (nonatomic,retain) NSMutableDictionary *requests;
@@ -21,18 +20,12 @@
 @property NSInteger finishCount;
 @property (retain) id delegate;
 
+-(id)initWithBlock:(completionhandler2)theblock AndDataUrl:(NSArray *)thearray AndUrlArray:(NSArray *)urlArray;
 - (id)initWithUrls:(NSArray *)aUrls;
 - (void)requestWithUrls:(NSArray *)aUrls;
 - (NSData *)dataAtIndex:(NSInteger)idx;
 - (NSString *)dataAsStringAtIndex:(NSInteger)idx;
 - (void)setDelegate:(id)val;
 - (id)delegate;
-
-@end
-
-@interface NSObject (MultipleDownloadDelegateMethods)
-
-- (void)didFinishDownload:(NSNumber*)idx;
-- (void)didFinishAllDownload;
-
+-(void)cancelAllrequests;
 @end
